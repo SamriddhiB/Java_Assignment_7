ee2253568036c249d665a6f6d55fe5b31bbe6d47
SamriddhiB <110332382+SamriddhiB@users.noreply.github.com>
2022-10-10 21:52:46 +0530

    Add files via upload

---
+++ prac7_1.java
@@ -0,0 +1,50 @@
+/*Create a generic method for sorting an array of Comparable objects.
+*/
+public class prac7_1 {
+    public static void main(String[] args) {
+
+        // making arrays
+        Integer[] intArray = { 5, 3, 4, 1, 2 };
+        Double[] doubleArray = { 3.0, 5.0, 1.0, 4.0, 2.0 };
+        Character[] characterArray = { 'B', 'D', 'E', 'C', 'A' };
+        // calling method
+        sort(intArray);
+        sort(doubleArray);
+        sort(characterArray);
+        // printing sorted output
+        System.out.print("Sorted integer: ");
+        printList(intArray);
+        System.out.print("Sorted double: ");
+        printList(doubleArray);
+        System.out.print("Sorted character: ");
+        printList(characterArray);
+    }
+
+    public static <M extends Comparable<M>> void sort(M[] list) {
+        M currentMin;
+        int currentMinIndex;
+        // loop to sort the given array
+        for (int i = 0; i < list.length - 1; i++) {
+            currentMin = list[i];
+            currentMinIndex = i;
+            for (int j = i + 1; j < list.length; j++) {
+                if (currentMin.compareTo(list[j]) > 0) {
+                    currentMin = list[j];
+                    currentMinIndex = j;
+                }
+            }
+            if (currentMinIndex != i) {
+                list[currentMinIndex] = list[i];
+                list[i] = currentMin;
+            }
+        }
+    }
+
+    public static void printList(Object[] list) {
+        // loop to print the sorted array elements
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ prac7_2.java
@@ -0,0 +1,41 @@
+/*Write a program that counts the occurrences of words in a text and displays the words and
+their occurrences in alphabetical order of the words. Using Map and Set Classes. 
+21ce011_SAMRIDDHI BHATT*/
+import java.util.*;
+public class prac7_2
+{
+public static void main(String[] args)
+{
+
+// taking variable of type String and intializing it
+String text = "Hello World!" + "Hello there!";
+Map<String, Integer> map = new TreeMap<>();
+String[] words = text.split("[ \n\t\r.,;:!?()]");
+// loop to calculate quantity of each word in given string
+for(int i=0; i< words.length; i++)
+{
+String key = words[i].toLowerCase();
+if(key.length() > 0)
+{
+if(!map.containsKey(key))
+{
+map.put(key, 1);
+}
+else
+{
+int value = map.get(key);
+value++;
+map.put(key, value);
+}
+}
+}
+// printing quantity of each word in given string
+Set<Map.Entry<String, Integer>> entrySet = map.entrySet();
+for(Map.Entry<String, Integer> entry : entrySet)
+{
+System.out.println("\n" + entry.getValue() + " word/s - " +
+
+entry.getKey());
+}
+}
+}
